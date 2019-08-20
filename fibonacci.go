0249925aae3b95ccadf4e05fbd57be9375bbd189
Chris Lemler <chris@lemler.org>
2019-08-19 23:17:46 -0700

    Initial import

---
+++ fibonacci.go
@@ -0,0 +1,30 @@
+// fibonacci - prints the first N numbers of the sequence
+package main
+
+import "fmt"
+
+// fibonacci returns a function which returns the next number (int)
+// in the Fibonacci sequence.
+//
+// It uses closure to track the next fibonacci number in the sequence.
+func fibonacci() func() int {
+	var fib = map[int]int{0: 0, 1: 1, 2: 1}
+	var index int = 0
+
+	return func() int {
+		v, ok := fib[index]
+		if !ok {
+			fib[index] = fib[index-1] + fib[index-2]
+		}
+		v = fib[index]
+		index++
+		return v
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
